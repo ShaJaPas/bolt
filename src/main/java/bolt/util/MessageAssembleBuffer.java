2a32c1902c8f4a5cc075798f71613601a654ccdf
Cian OM <cinaom@gmail.com>
2016-03-02 09:39:43 +0000

    Implementation for MessageAssembleBuffer

--- src/main/java/bolt/util/MessageAssembleBuffer.java
+++ src/main/java/bolt/util/MessageAssembleBuffer.java
@@ -2,8 +2,11 @@ package bolt.util;
 
 import bolt.packets.DataPacket;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by keen on 29/02/16.
@@ -15,22 +18,37 @@ public class MessageAssembleBuffer
 
     private volatile int messageId = 0;
 
+    private Map<Integer, Message> messageMap = new HashMap<>();
+
     public Collection<DataPacket> addChunk(final DataPacket dataPacket) {
         if (!dataPacket.isMessage()) {
             return Collections.singletonList(dataPacket);
         }
         else {
-            //TODO implement
-            // Add chunk
-
-            // If all chunks exist, return them as collection
-
-            // Else return empty list
+            messageMap.putIfAbsent(dataPacket.getMessageId(), new Message());
+            return messageMap.get(dataPacket.getMessageId()).addChunk(dataPacket);
         }
     }
 
     public int nextMessageId() {
         return SequenceNumber.increment(messageId, MAX_MESSAGE_ID);
+    }
+
+
+    private static class Message {
+        private final Map<Integer, DataPacket> received = new HashMap<>();
+        private int totalChunks;
+
+        private Collection<DataPacket> addChunk(final DataPacket packet) {
+            received.put(packet.getMessageChunkNumber(), packet);
+            if (packet.isFinalMessageChunk()) {
+                totalChunks = packet.getMessageChunkNumber();
+            }
+            return totalChunks == received.size()
+                    ? new ArrayList<>(received.values())
+                    : Collections.emptyList();
+        }
+
     }
 
 }
