16da410b8d3df9b10465e0336a6de2d46d637ee5
cianom <cianom@gmail.com>
2016-03-15 00:31:05 +0000

    - Refactoring tests package

--- src/main/java/bolt/BoltServer.java
+++ src/main/java/bolt/BoltServer.java
@@ -103,4 +103,8 @@ public class BoltServer implements Server {
         }
     }
 
+    public Config config() {
+        return config;
+    }
+
 }
--- src/test/java/bolt/TestBoltServer.java
+++ src/test/java/bolt/TestBoltServer.java
@@ -3,7 +3,6 @@ package bolt;
 import bolt.util.TestUtil;
 import org.junit.Test;
 
-import java.net.InetAddress;
 import java.security.MessageDigest;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,7 +11,6 @@ import static org.junit.Assert.assertEquals;
 
 public class TestBoltServer extends BoltTestBase {
 
-    boolean running = false;
     int num_packets = 32;
     long total = 0;
     volatile boolean serverRunning = true;
@@ -46,10 +44,8 @@ public class TestBoltServer extends BoltTestBase {
     }
 
     protected void doTest(final float packetLossPercentage) throws Throwable {
-        final Config clientConfig = new Config(InetAddress.getByName("localhost"), nextClientPort());
-        final Config serverConfig = new Config(InetAddress.getByName("localhost"), nextServerPort())
-                .setPacketLoss(packetLossPercentage);
-        if (!running) runServer();
+        final BoltServer server = runServer();
+        server.config().setPacketLoss(packetLossPercentage);
 
         final int N = num_packets * 32768;
         final byte[] data = getRandomData(N);
@@ -57,7 +53,7 @@ public class TestBoltServer extends BoltTestBase {
         final long start = System.currentTimeMillis();
         final Set<Throwable> errors = new HashSet<>();
 
-        final BoltClient client = runClient(serverConfig.getLocalPort(),
+        final BoltClient client = runClient(server.getPort(),
                 c -> {
                     System.out.println("Sending data block of <" + N / 1024 + "> Kbytes.");
                     c.sendBlocking(data);
@@ -80,9 +76,9 @@ public class TestBoltServer extends BoltTestBase {
         assertEquals(md5_sent, md5_received);
     }
 
-    private void runServer() throws Exception {
+    private BoltServer runServer() throws Exception {
         serverMd5 = MessageDigest.getInstance("MD5");
-        runServer(byte[].class, x -> {
+        return runServer(byte[].class, x -> {
             serverMd5.update(x, 0, x.length);
             total += x.length;
         }, ex -> {
